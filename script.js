9745666fbad3a6d139d01a527edc7ecb1e709825
Ashokas <99681959+Ashokaas@users.noreply.github.com>
2022-09-06 01:13:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 
+// https://stackoverflow.com/questions/17749681/get-xmlhttprequest-not-working
+function load_google_sheets(url) {
+    if (window.XMLHttpRequest) {
+      var xmlhttp=new XMLHttpRequest();
+    }
+    else {
+      var xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET",url,false);
+    xmlhttp.send(null);
+    console.log(xmlhttp.responseText)
+    return xmlhttp.responseText;
+}
+
+
+
+// https://stackoverflow.com/questions/27979002/convert-csv-data-into-json-format-using-javascript
+function csv_to_JSON(csv) {
+  const lines = csv.split('\r\n')
+  const result = []
+  const headers = lines[0].split(',')
+
+  for (let i = 1; i < lines.length; i++) {        
+      if (!lines[i])
+          continue
+      const obj = {}
+      const currentline = lines[i].split(',')
+
+      for (let j = 0; j < headers.length; j++) {
+          obj[headers[j]] = currentline[j]
+      }
+      result.push(obj)
+  }
+  return result
+}
+
+
+
+
+
+
 function rechercher() {
-    var manuels = [
-        {
-          "url": "https://biblio.nathan.fr/demo/9782091317243/",
-          "classe": "term",
-          "matiere": "maths",
-          "edition": "nathan",
-          "titre": "Hyperbole Terminale - Option Maths Expertes"
-        }
-      ]
+    var manuels = csv_to_JSON(load_google_sheets("https://docs.google.com/spreadsheets/d/e/2PACX-1vTd1-ZFtLOY49L8MiceYW6Btl0i_5kdSpqWvaor_bxQX9z1QEkcANBX6Qct7aYArBkaYGgPbQ-OtFr8/pub?gid=0&single=true&output=csv"))
+    console.log(manuels)
+    
     var classe = document.getElementById('classe').value;
     var matiere = document.getElementById('matiere').value;
     var edition = document.getElementById('edition').value;
@@ -20,6 +55,7 @@ function rechercher() {
             var bon_manuel = manuels[i];
         }
     }
+    console.log(bon_manuel)
     // Create anchor element.
     var a = document.createElement('a'); 
                   
@@ -41,4 +77,9 @@ function rechercher() {
 
 // Execute la fonction rechercher() lors du click sur le bouton
 document.getElementById("btn_rechercher").addEventListener('click', rechercher);
+
+
+
+
+
 
